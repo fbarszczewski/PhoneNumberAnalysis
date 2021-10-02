5b26ab935ee68605b30a38a21bd9a8cf65fded07
filip barszczewski <fbarszczewski@outlook.com>
2021-10-02 16:07:23 +0200

    Analyze

--- PhoneNumberAnalysis.cs
+++ PhoneNumberAnalysis.cs
@@ -1,14 +1,19 @@
 using System;
 
+
 public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+
+        return
+            (string.Compare("212", 0, phoneNumber, 0, 3) == 0,
+                string.Compare("555", 0, phoneNumber, 4, 3) == 0,
+                phoneNumber.Substring(phoneNumber.Length-4));
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+
     }
 }
